def26cf49ad44dd2116da1578d700999df8e8424
HASHIMOTO Wataru <watr@users.noreply.github.com>
2016-04-12 16:27:00 +0900

    add logging demo with syslog

---
+++ sendsyslog/sendsyslog/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sendsyslog
+//
+//  Created by HASHIMOTO Wataru on 4/12/16.
+//  Copyright © 2016 HASHIMOTO Wataru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
