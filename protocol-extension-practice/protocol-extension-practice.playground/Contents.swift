cdbec01432fc55ed05e0d93e1c91fa81e1a56412
HASHIMOTO Wataru <watr@users.noreply.github.com>
2016-04-26 18:38:37 +0900

    Add code with class, and modify put text.

--- protocol-extension-practice/protocol-extension-practice.playground/Contents.swift
+++ protocol-extension-practice/protocol-extension-practice.playground/Contents.swift
@@ -2,17 +2,24 @@
 import UIKit
 
 protocol HelloProtocol {
+    //func say()
 }
 
 extension HelloProtocol {
     func say() {
-        print("protocol")
+        print("protocol-extension")
     }
 }
 
-struct HelloStruct : HelloProtocol {
+struct HelloStruct: HelloProtocol {
     func say() {
         print("stcuct")
+    }
+}
+
+class HelloClass: HelloProtocol {
+    func say() {
+        print("class")
     }
 }
 
@@ -20,8 +27,20 @@ struct HelloStruct : HelloProtocol {
 // demo
 /////////////////////////////////////////////
 
-let s: HelloStruct = HelloStruct()
-s.say()
+do {
+    let s: HelloStruct = HelloStruct()
+    s.say()
+    
+    let p: HelloProtocol = s
+    p.say()
+}
 
-let p: HelloProtocol = s
-p.say()
+print("========")
+
+do {
+    let c: HelloClass = HelloClass()
+    c.say()
+    
+    let p: HelloProtocol = c
+    p.say()
+}
