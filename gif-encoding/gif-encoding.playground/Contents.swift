b01e5638e4f321a2a16f3379476508ec54e6c0e7
HASHIMOTO Wataru <watr@users.noreply.github.com>
2016-04-26 13:10:17 +0900

    Add gif encoding practice.

---
+++ gif-encoding/gif-encoding.playground/Contents.swift
@@ -0,0 +1,116 @@
+
+import UIKit
+import ImageIO
+import MobileCoreServices
+
+enum GIFLoopCount {
+    case Forever
+    case Count(count: UInt16)
+    
+    var rawValue: UInt16 {
+        switch self {
+        case .Forever:
+            return 0
+        case .Count(let count):
+            return count
+        }
+    }
+}
+
+struct GIFFrame {
+    let image: CGImage
+    let duration: CGFloat
+}
+
+protocol GIFEncoderDataSource {
+    var gifFrameCount: Int {get}
+    var loopCount: GIFLoopCount {get}
+    func gifFrame(index: Int) -> GIFFrame
+}
+
+class GIFEncoder {
+    let dataSource: GIFEncoderDataSource
+    init(dataSource: GIFEncoderDataSource) {
+        self.dataSource = dataSource
+    }
+    func encode(completion:(encodedData: NSData?) -> Void) {
+        let data = CFDataCreateMutable(kCFAllocatorDefault, 0)!
+        let frameCount = dataSource.gifFrameCount
+        let loopCount = dataSource.loopCount
+        
+        let destination = CGImageDestinationCreateWithData(data,
+                                                           kUTTypeGIF,
+                                                           frameCount,
+                                                           nil)
+        
+        for i in 0..<frameCount {
+            let gifFrame = dataSource.gifFrame(i)
+            
+            let dic: NSDictionary = [kCGImagePropertyGIFDictionary as NSString: ([kCGImagePropertyGIFDelayTime as NSString: 0.5] as NSDictionary)]
+            
+            CGImageDestinationAddImage(destination!, gifFrame.image, dic)
+        }
+        
+        do {
+            let dic = [kCGImagePropertyGIFDictionary as NSString: ([kCGImagePropertyGIFLoopCount as NSString: NSNumber(unsignedShort: loopCount.rawValue)] as NSDictionary)]
+            CGImageDestinationSetProperties(destination!, dic)
+        }
+        CGImageDestinationFinalize(destination!)
+        
+        completion(encodedData:data as NSData)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+// demo
+////////////////////////////////////////////////////////////////////////
+
+struct SomeWhatContainer {
+    let recImage = UIImage(named: "rec.png")
+    let blankImage = UIImage(named: "blank.png")
+}
+
+extension SomeWhatContainer: GIFEncoderDataSource {
+    var gifFrameCount: Int {
+        get {
+            return 2
+        }
+    }
+    
+    var loopCount: GIFLoopCount {
+        get{
+            return .Count(count: 5)
+        }
+    }
+    
+    func gifFrame(index: Int) -> GIFFrame {
+        let rec = (index % 2 == 0)
+        
+        let image = (rec ? self.recImage : self.blankImage)
+        return GIFFrame(image: (image?.CGImage)!, duration:(rec ? 0.5 : 0.1))
+    }
+}
+
+let encoder = GIFEncoder(dataSource: SomeWhatContainer())
+
+encoder.encode() { data in
+    if let dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true).first {
+        
+        // print help for preview gif file
+        func printHelp(url: NSURL) {
+            print("open -a Safari \"\(url)\"")
+        }
+        
+        let file = "demo.gif"
+        let url = NSURL(fileURLWithPath: dir).URLByAppendingPathComponent(file)
+        
+        // print help
+        printHelp(url)
+        
+        do {
+            try data?.writeToURL(url, options: .DataWritingAtomic)
+        }
+        catch {
+        }
+    }
+}
