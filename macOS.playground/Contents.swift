0991d50ce31c6a3cf316e6f9ff0feba5f6923db6
HASHIMOTO Wataru <watr@users.noreply.github.com>
2018-12-27 10:12:19 +0900

    Compare version string

---
+++ macOS.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+["1.0",
+ "2.0",
+ "2.2.0",
+ "2.3.0",
+ "2.3.3",
+ "2.10.0"].forEach({
+    let result = ("2.3.0".compare($0,
+                                  options: [.numeric]))
+    print((result != .orderedDescending))
+ })
